c46c5ee9994c04622386c2770d1afd1cc7db0b39
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-12-06 08:51:31 +0100

    Filter files only

--- scripts/version.js
+++ scripts/version.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 
-const from = process.argv[1];
-const to = process.argv[2];
+const from = process.argv[2];
+const to = process.argv[3];
 
 let fromStdIn = '';
 
-console.log(`replacing '${from}' to '${to}'`);
+console.log(`replacing '${from}' to '${to}' on :\n`);
 
 process.stdin.on('data', (data) => fromStdIn = fromStdIn + data);
 process.stdin.on('end', () => {
-  fromStdIn.split('\n').map(line => {
-    const content = fs.readFileSync(line).toString('utf8');
-    fs.writeFileSync(line, content.replace(new RegExp(from.replace(new RegExp('\\.', 'g'), '\\.'), 'g'), to));
+  fromStdIn.split('\n').map(line => path.join(process.cwd(), line)).filter(fp => fs.existsSync(fp)).filter(fp => fs.statSync(fp).isFile()).map(fullPath => {
+    console.log(` * ${fullPath}`);
+    const content = fs.readFileSync(fullPath).toString('utf8');
+    fs.writeFileSync(fullPath, content.replace(new RegExp(from.replace(new RegExp('\\.', 'g'), '\\.'), 'g'), to));
   });
 });
