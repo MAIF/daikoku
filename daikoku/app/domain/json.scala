00c69e984e923b4cc8e7a69b8beaa5d5554a18bd
Quentin <quentino.vega@gmail.com>
2020-07-16 16:40:17 +0200

    Format sources before release

--- daikoku/app/domain/json.scala
+++ daikoku/app/domain/json.scala
@@ -883,8 +883,9 @@ object json {
               .asOpt[Boolean]
               .getOrElse(false),
             metadata = (json \ "metadata").asOpt[JsObject].getOrElse(Json.obj()),
-            customMetadata =
-              (json \ "customMetadata").asOpt(SeqCustomMetadataFormat).getOrElse(Seq.empty),
+            customMetadata = (json \ "customMetadata")
+              .asOpt(SeqCustomMetadataFormat)
+              .getOrElse(Seq.empty),
             tags = (json \ "tags").asOpt[JsArray].getOrElse(Json.arr()),
             restrictions = (json \ "restrictions").as(ApiKeyRestrictionsFormat),
           )
--- daikoku/javascript/src/components/utils/entities.js
+++ daikoku/javascript/src/components/utils/entities.js
@@ -35,6 +35,5 @@ export const newPossibleUsagePlan = (customName) => ({
         notFound: [],
       },
     },
-    
   },
 });
