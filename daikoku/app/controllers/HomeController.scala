b9c4147dc89d5a674dbcc5a81050ae643fd7b20c
Quentin <quentino.vega@gmail.com>
2020-12-11 16:14:10 +0100

    Add an api to check daikoku health

--- daikoku/app/controllers/HomeController.scala
+++ daikoku/app/controllers/HomeController.scala
@@ -1,17 +1,16 @@
 package fr.maif.otoroshi.daikoku.ctrls
 
-import fr.maif.otoroshi.daikoku.actions.{
-  DaikokuActionMaybeWithoutUser,
-  DaikokuActionMaybeWithoutUserContext
-}
+import fr.maif.otoroshi.daikoku.actions.{DaikokuAction, DaikokuActionMaybeWithGuest, DaikokuActionMaybeWithoutUser, DaikokuActionMaybeWithoutUserContext}
 import fr.maif.otoroshi.daikoku.env.Env
+import fr.maif.otoroshi.daikoku.logger.AppLogger
 import play.api.mvc._
 
 class HomeController(
-    DaikokuActionMaybeWithoutUser: DaikokuActionMaybeWithoutUser,
-    env: Env,
-    cc: ControllerComponents)
-    extends AbstractController(cc) {
+                      DaikokuActionMaybeWithoutUser: DaikokuActionMaybeWithoutUser,
+                      DaikokuActionMaybeWithGuest: DaikokuActionMaybeWithGuest,
+                      env: Env,
+                      cc: ControllerComponents)
+  extends AbstractController(cc) {
 
   implicit val ec = env.defaultExecutionContext
 
@@ -20,12 +19,12 @@ class HomeController(
       case Some(_) =>
         Ok(
           views.html.index(ctx.user.get,
-                           ctx.session.get,
-                           ctx.tenant,
-                           ctx.request.domain,
-                           env,
-                           ctx.isTenantAdmin,
-                           ctx.apiCreationPermitted))
+            ctx.session.get,
+            ctx.tenant,
+            ctx.request.domain,
+            env,
+            ctx.isTenantAdmin,
+            ctx.apiCreationPermitted))
       case None if ctx.request.uri.startsWith("/signup") =>
         Ok(views.html.unauthenticatedindex(ctx.tenant, ctx.request.domain, env))
       case None if ctx.request.uri.startsWith("/reset") =>
@@ -42,5 +41,14 @@ class HomeController(
 
   def indexWithPath(path: String) = DaikokuActionMaybeWithoutUser { ctx =>
     actualIndex(ctx)
+  }
+
+  def health() = DaikokuActionMaybeWithGuest { ctx =>
+    ctx.request.headers.get("Otoroshi-Health-Check-Logic-Test")
+    match {
+        //todo: better health check
+      case Some(value) => Ok.withHeaders("Otoroshi-Health-Check-Logic-Test-Result" -> (value.toLong + 42L).toString)
+      case None => BadRequest
+    }
   }
 }
