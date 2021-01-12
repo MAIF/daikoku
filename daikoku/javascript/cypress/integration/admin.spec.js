f75ef572703d0927150dd002f02695aabdd904e1
Quentin <quentino.vega@gmail.com>
2021-01-12 16:14:12 +0100

    Add some admin end2end test

--- daikoku/javascript/cypress/integration/admin.spec.js
+++ daikoku/javascript/cypress/integration/admin.spec.js
@@ -1,8 +1,8 @@
 describe('Login', () => {
-  it ('login', () => {
+  it('login', () => {
     cy
       .clearCookie('daikoku-session')
-      .visit('http://localhost:9000')  
+      .visit('http://localhost:9000')
       .get('a.btn').contains('Connect').click()
       .get('input[name=username]').type('admin@foo.bar')
       .get('input[name=password]').type('password')
@@ -22,7 +22,7 @@ describe('Messages page', () => {
 });
 
 describe('Oto instances page', () => {
-  it('Team income works', () => {
+  it('load well', () => {
     cy
       .get('nav#sidebar a.nav-link').contains('Otoroshi instances').click()
       .url().should('include', '/settings/otoroshis')
@@ -30,6 +30,51 @@ describe('Oto instances page', () => {
       .get('table tbody tr .btn-outline-primary').click()
       .url().should('include', '/settings/otoroshis/default')
       .get('main form').should('be.visible');
+  });
+});
+
+describe('Admins page', () => {
+  it('load well', () => {
+    cy
+      .get('nav#sidebar a.nav-link').contains('Admins').click()
+      .url().should('include', '/settings/admins')
+      .get('.avatar-with-action').should('have.length', 1);
+  });
+});
+
+describe('Audit trail page', () => {
+  it('load well', () => {
+    cy
+      .get('nav#sidebar a.nav-link').contains('Audit trail').click()
+      .url().should('include', '/settings/audit')
+      .get('table').should('is.visible');
+  });
+});
+
+describe('teams page', () => {
+  it('load well', () => {
+    cy
+      .get('nav#sidebar a.nav-link').contains('Teams').click()
+      .url().should('include', '/settings/teams')
+      .get('.avatar-with-action').should('have.length', 5); //todo: test edit && memebers
+  });
+});
+
+describe('tenants page', () => {
+  it('load well', () => {
+    cy
+      .get('nav#sidebar a.nav-link').contains('Tenants').click()
+      .url().should('include', '/settings/tenants')
+      .get('.avatar-with-action').should('have.length', 1); //todo: test edit
+  });
+});
+
+describe('users page', () => {
+  it('load well', () => {
+    cy
+      .get('nav#sidebar a.nav-link').contains('Users').click()
+      .url().should('include', '/settings/users')
+      .get('.avatar-with-action').should('have.length', 2); //todo: test edit
   });
 });
 
