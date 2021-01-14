efe8b24b7010eaddd5a5ef6cf3a6d1d5c465e856
Quentin <quentino.vega@gmail.com>
2021-01-14 16:44:45 +0100

    ヽ(｀Д´)ノ It Will work now

--- daikoku/javascript/cypress/support/index.js
+++ daikoku/javascript/cypress/support/index.js
@@ -15,20 +15,10 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import addContext from 'mochawesome/addContext';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
 Cypress.Cookies.defaults({
   preserve: 'daikoku-session'
-});
-
-Cypress.on('test:after:run', (test, runnable) => {
-
-  let videoName = Cypress.spec.name;
-  videoName = videoName.replace('/.js.*', '.js');
-  const videoUrl = 'videos/' + videoName + '.mp4';
-
-  addContext({ test }, videoUrl);
 });
