f7fd04c561054c594f806b9c539008fd05bbd83f
Quentin <quentino.vega@gmail.com>
2021-06-10 14:44:03 +0200

    FIX UI tests

---
+++ daikoku/javascript/src/components/utils/permissions/subjects.js
@@ -0,0 +1,8 @@
+export const apikey = 'APIKEY';
+export const asset = 'ASSET';
+export const api = 'API';
+export const stat = 'STAT';
+export const team = 'TEAM';
+export const tenant = 'TENANT';
+export const daikoku = 'DAIKOKU';
+export const backoffice = 'BACKOFFICE';
