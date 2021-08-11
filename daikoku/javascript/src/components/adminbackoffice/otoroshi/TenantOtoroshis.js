0e397cf9a291be35f9d0e45bc11405e0efb7a810
Etienne ANNE <etienne.anne@icloud.com>
2021-08-11 17:24:22 +0200

    rewrite tenantOtoroshis component

--- daikoku/javascript/src/components/adminbackoffice/otoroshi/TenantOtoroshis.js
+++ daikoku/javascript/src/components/adminbackoffice/otoroshi/TenantOtoroshis.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { v4 as uuid } from 'uuid';
@@ -8,27 +8,28 @@ import * as Services from '../../../services';
 import { Table } from '../../inputs';
 import { UserBackOffice } from '../../backoffice';
 import { Can, manage, tenant } from '../../utils';
-import { t, Translation } from '../../../locales';
+import { Translation } from '../../../locales';
 import { toastr } from 'react-redux-toastr';
+import { I18nContext } from '../../../core/context';
 
-export class TenantOtoroshisComponent extends Component {
-  state = {
-    otoroshis: [],
-  };
+export function TenantOtoroshisComponent(props) {
+  const { translateMethod } = useContext(I18nContext);
 
-  columns = [
+  let table;
+
+  const columns = [
     {
-      Header: t('Url', this.props.currentLanguage),
+      Header: translateMethod('Url'),
       style: { textAlign: 'left' },
       accessor: (item) => item.url,
     },
     {
-      Header: t('Host', this.props.currentLanguage),
+      Header: translateMethod('Host'),
       style: { textAlign: 'left' },
       accessor: (item) => item.host,
     },
     {
-      Header: t('Actions', this.props.currentLanguage),
+      Header: translateMethod('Actions'),
       style: { textAlign: 'center' },
       disableSortBy: true,
       disableFilters: true,
@@ -41,22 +42,22 @@ export class TenantOtoroshisComponent extends Component {
         const otoroshi = original;
         return (
           <div className="btn-group">
-            {this.isTenantAdmin() && (
+            {isTenantAdmin() && (
               <Link to={`/settings/otoroshis/${otoroshi._id}`}>
                 <button
                   type="button"
                   className="btn btn-sm btn-outline-primary"
-                  title={t('Edit this settings', this.props.currentLanguage)}>
+                  title={translateMethod('Edit this settings')}>
                   <i className="fas fa-edit" />
                 </button>
               </Link>
             )}
-            {this.isTenantAdmin() && (
+            {isTenantAdmin() && (
               <button
                 type="button"
                 className="btn btn-sm btn-outline-danger"
-                title={t('Delete this settings', this.props.currentLanguage)}
-                onClick={() => this.delete(otoroshi._id)}>
+                title={translateMethod('Delete this settings')}
+                onClick={() => onDelete(otoroshi._id)}>
                 <i className="fas fa-trash" />
               </button>
             )}
@@ -66,41 +67,39 @@ export class TenantOtoroshisComponent extends Component {
     },
   ];
 
-  isTenantAdmin = () => {
-    if (this.props.connectedUser.isDaikokuAdmin) {
+  const isTenantAdmin = () => {
+    if (props.connectedUser.isDaikokuAdmin) {
       return true;
     }
-    return this.props.tenant.admins.indexOf(this.props.connectedUser._id) > -1;
+    return props.tenant.admins.indexOf(props.connectedUser._id) > -1;
   };
 
-  delete = (id) => {
+  const onDelete = (id) => {
     window
       .confirm(
         t(
           'otoroshi.settings.delete.confirm',
-          this.props.currentLanguage,
           false,
           'Are you sure you want to delete those otoroshi settings ?'
         )
       )
       .then((ok) => {
         if (ok) {
-          Services.deleteOtoroshiSettings(this.props.tenant._id, id).then(() => {
+          Services.deleteOtoroshiSettings(props.tenant._id, id).then(() => {
             toastr.success(
               t(
                 'otoroshi.settings.deleted.success',
-                this.props.currentLanguage,
                 false,
                 'Otoroshi settings successfuly deleted'
               )
             );
-            this.table.update();
+            table.update();
           });
         }
       });
   };
 
-  createNewSettings = () => {
+  const createNewSettings = () => {
     const settings = {
       _id: uuid(),
       url: 'https://otoroshi-api.foo.bar',
@@ -108,52 +107,50 @@ export class TenantOtoroshisComponent extends Component {
       clientId: faker.random.alphaNumeric(16),
       clientSecret: faker.random.alphaNumeric(64),
     };
-    this.props.history.push(`/settings/otoroshis/${settings._id}`, { newSettings: settings });
+    props.history.push(`/settings/otoroshis/${settings._id}`, { newSettings: settings });
   };
 
-  render() {
-    return (
-      <UserBackOffice tab="Otoroshi">
-        <Can I={manage} a={tenant} dispatchError>
-          <div className="row">
-            <div className="col">
-              <h1>
-                <Translation i18nkey="Otoroshi settings">
-                  Otoroshi settings
-                </Translation>
-                <a
-                  className="btn btn-sm btn-access-negative mb-1 ml-1"
-                  title={t('Create new settings', this.props.currentLanguage)}
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    this.createNewSettings();
-                  }}>
-                  <i className="fas fa-plus-circle" />
-                </a>
-              </h1>
-              <div className="section p-2">
-                <Table
-                  currentLanguage={this.props.currentLanguage}
-                  selfUrl="otoroshis"
-                  defaultTitle="Otoroshi instances"
-                  defaultValue={() => ({})}
-                  defaultSort="Url"
-                  itemName="otoroshi"
-                  columns={this.columns}
-                  fetchItems={() => Services.allOtoroshis(this.props.tenant._id)}
-                  showActions={false}
-                  showLink={false}
-                  extractKey={(item) => item._id}
-                  injectTable={(t) => (this.table = t)}
-                />
-              </div>
+  return (
+    <UserBackOffice tab="Otoroshi">
+      <Can I={manage} a={tenant} dispatchError>
+        <div className="row">
+          <div className="col">
+            <h1>
+              <Translation i18nkey="Otoroshi settings">
+                Otoroshi settings
+              </Translation>
+              <a
+                className="btn btn-sm btn-access-negative mb-1 ml-1"
+                title={translateMethod('Create new settings')}
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  createNewSettings();
+                }}>
+                <i className="fas fa-plus-circle" />
+              </a>
+            </h1>
+            <div className="section p-2">
+              <Table
+                currentLanguage={props.currentLanguage}
+                selfUrl="otoroshis"
+                defaultTitle="Otoroshi instances"
+                defaultValue={() => ({})}
+                defaultSort="Url"
+                itemName="otoroshi"
+                columns={columns}
+                fetchItems={() => Services.allOtoroshis(props.tenant._id)}
+                showActions={false}
+                showLink={false}
+                extractKey={(item) => item._id}
+                injectTable={(t) => (table = t)}
+              />
             </div>
           </div>
-        </Can>
-      </UserBackOffice>
-    );
-  }
+        </div>
+      </Can>
+    </UserBackOffice>
+  );
 }
 
 const mapStateToProps = (state) => ({
