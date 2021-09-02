5be48a570eb4adf312ee9bd007e18318c876d29a
Etienne ANNE <etienne.anne@icloud.com>
2021-09-02 10:52:53 +0200

    Add missing imports

--- daikoku/javascript/src/components/frontend/api/ApiHome.js
+++ daikoku/javascript/src/components/frontend/api/ApiHome.js
@@ -290,7 +290,7 @@ const ApiHomeComponent = ({
         <h1 style={{ margin: 0 }}>{showAccessModal.error}</h1>
         {(teams.length === 1 &&
           (pendingTeams.includes(teams[0]._id) || authorizedTeams.includes(teams[0]._id))) ||
-        showAccessModal.api.authorizations.every((auth) => auth.pending && !auth.authorized) ? (
+          showAccessModal.api.authorizations.every((auth) => auth.pending && !auth.authorized) ? (
           <>
             <h2 className="text-center my-3">{translateMethod('request_already_pending')}</h2>
             <button
@@ -377,9 +377,8 @@ const ApiHomeComponent = ({
               </li>
               <li className="nav-item">
                 <Link
-                  className={`nav-link ${
-                    tab === 'documentation' || tab === 'documentation-page' ? 'active' : ''
-                  }`}
+                  className={`nav-link ${tab === 'documentation' || tab === 'documentation-page' ? 'active' : ''
+                    }`}
                   to={`/${match.params.teamId}/${apiId}/${versionId}/documentation`}>
                   <Translation i18nkey="Documentation">Documentation</Translation>
                 </Link>
@@ -523,8 +522,8 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = {
-  setError,
-  openContactModal,
+  setError: (e) => setError(e),
+  openContactModal: (props) => openContactModal(props),
   updateUser: (u) => updateUser(u)
 };
 
