508ed990521d922a06c211cf6fdf0caf7834825e
Quentin <quentino.vega@gmail.com>
2022-06-08 14:41:11 +0200

    FIX usage of Switch button on  import/export page

--- daikoku/javascript/src/components/adminbackoffice/importexport/ImportExport.js
+++ daikoku/javascript/src/components/adminbackoffice/importexport/ImportExport.js
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React, { useContext, useState } from 'react';
+import { BooleanInput } from '@maif/react-forms';
+
 import * as Services from '../../../services';
 import { Can, manage, daikoku } from '../../utils';
-import { SwitchButton } from '../../inputs';
 import { I18nContext } from '../../../locales/i18n-context';
 import { useDaikokuBackOffice } from '../../../contexts';
 
@@ -14,13 +15,11 @@ export const ImportExport = () => {
   let input;
 
   const [uploading, setUploading] = useState(false);
-  const [state, setState] = useState({
-    exportAuditTrail: true,
-    migration: {
-      processing: false,
-      error: '',
-      onSuccessMessage: '',
-    },
+  const [exportAuditTrail, setExportAuditTrail] = useState(true);
+  const [migration, setMigration] = useState({
+    processing: false,
+    error: '',
+    onSuccessMessage: '',
   });
 
   const importState = () => {
@@ -39,27 +38,20 @@ export const ImportExport = () => {
   };
 
   const migrate = () => {
-    setState({
-      ...state,
-      migration: {
-        processing: true,
-        error: '',
-        onSuccessMessage: '',
-      },
+    setMigration({
+      processing: true,
+      error: '',
+      onSuccessMessage: '',
     });
-    Services.migrateMongoToPostgres().then((res) => {
-      setState({
-        ...state,
-        migration: {
-          processing: false,
-          error: res.error || '',
-          onSuccessMessage: res.error ? '' : res.message,
-        },
-      });
-    });
+    Services.migrateMongoToPostgres()
+      .then((res) => setMigration({
+        processing: false,
+        error: res.error || '',
+        onSuccessMessage: res.error ? '' : res.message,
+      }));
   };
 
-  const { processing, error, onSuccessMessage } = state.migration;
+  const { processing, error, onSuccessMessage } = migration;
   return (
     <Can I={manage} a={daikoku} dispatchError>
       <div className="row">
@@ -69,7 +61,7 @@ export const ImportExport = () => {
           </h1>
           <div className="section p-3">
             <a
-              href={`/api/state/export?download=true&export-audit-trail=${!!state.exportAuditTrail}`}
+              href={`/api/state/export?download=true&export-audit-trail=${!!exportAuditTrail}`}
               target="_blank"
               className="btn btn-outline-primary"
             >
@@ -85,12 +77,9 @@ export const ImportExport = () => {
               <i className="fas fa-upload me-1" />
               {uploading ? translateMethod('importing ...') : translateMethod('import state')}
             </button>
-            <div className="d-flex justify-content-start mt-2">
-              <SwitchButton
-                onSwitch={(enabled) => setState({ ...state, exportAuditTrail: enabled })}
-                checked={state.exportAuditTrail}
-                label={translateMethod('audittrail.export.label')}
-              />
+            <div className="d-flex justify-content-start align-items-center mt-2">
+              <label className='me-3'>{translateMethod('audittrail.export.label')}</label>
+              <BooleanInput onChange={setExportAuditTrail} value={exportAuditTrail} />
             </div>
             <input
               type="file"
