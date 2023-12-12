4e997565fcf44d0bffb0890171f0a72188fda1d9
Quentin Aubert <quentino.vega@gmail.com>
2023-12-12 10:11:16 +0100

    update manual

--- manual/docusaurus.config.js
+++ manual/docusaurus.config.js
@@ -136,10 +136,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/maif/daikoku'
               },
