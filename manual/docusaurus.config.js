c66e28d03323e6ab11096ca8782915fc897d277a
Quentin Aubert <quentino.vega@gmail.com>
2023-12-12 10:20:30 +0100

    update manual

--- manual/docusaurus.config.js
+++ manual/docusaurus.config.js
@@ -25,15 +25,15 @@ const config = {
   favicon: 'img/daikoku-mini.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://maif.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'MAIF', // Usually your GitHub org/user name.
+  projectName: 'Daikoku', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
