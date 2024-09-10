2268caf0017a63d48f08c5b8742c91179ff63c9f
baudelotphilippe <philippe@baudelot.eu>
2024-09-10 16:49:14 +0200

    horizontal center align for team label and logo

--- daikoku/javascript/src/components/frontend/api/ApiCard.tsx
+++ daikoku/javascript/src/components/frontend/api/ApiCard.tsx
@@ -103,7 +103,7 @@ export const ApiCard = (props: {
             <span className="flex-grow-1 api-description my-2">{api.smallDescription}</span>
             {props.teamVisible && team && (
               <small
-                className="cursor-pointer underline-on-hover a-fake d-flex align-items-baseline justify-content-end"
+                className="cursor-pointer underline-on-hover a-fake d-flex align-items-center justify-content-end"
                 onClick={() => props.handleTeamSelect(team)}
               >
                 <img alt="avatar" src={team.avatar} style={{ marginRight: 5, width: 20 }} />
@@ -181,7 +181,7 @@ export const ApiCard = (props: {
       <div className="col-12 d-flex mt-2">
         {props.teamVisible && team && (
           <small
-            className="cursor-pointer underline-on-hover level2-link d-flex align-items-baseline"
+            className="cursor-pointer underline-on-hover level2-link d-flex align-items-center"
             onClick={() => props.handleTeamSelect(team)}
           >
             <img alt="avatar" src={team.avatar} style={{ marginRight: 5, width: 20 }} />
