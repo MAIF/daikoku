621dc066ff28db14ecc8206ed660b9181710a7b9
Quentin AUBERT <quentino.vega@gmail.com>
2025-05-12 16:25:02 +0200

    FIX evolution 18.2.0 - it could fail if previous version is older than 17.5.0

--- daikoku/app/domain/json.scala
+++ daikoku/app/domain/json.scala
@@ -1587,7 +1587,9 @@ object json {
           )
         )
       } recover {
-        case e => JsError(e.getMessage)
+        case e =>
+          AppLogger.error(e.getMessage, e)
+          JsError(e.getMessage)
       } get
 
     override def writes(o: DaikokuStyle): JsValue =
