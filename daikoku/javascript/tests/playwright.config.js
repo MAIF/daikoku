4b304d8e8856157f329121a96992a23dbcdc0cac
Quentin AUBERT <quentino.vega@gmail.com>
2026-01-28 12:02:22 +0100

    avoid playwright to run test in all navigators

--- daikoku/javascript/tests/playwright.config.js
+++ daikoku/javascript/tests/playwright.config.js
@@ -40,14 +40,14 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 });
