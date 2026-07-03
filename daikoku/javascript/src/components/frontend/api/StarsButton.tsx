88ca3d7be3ce2df57a3b04255e206032362239cb
Quentin Aubert <quentino.vega@gmail.com>
2026-07-03 14:59:30 +0200

    style: beautifule star button & add gap in subscrioption column in apilist

--- daikoku/javascript/src/components/frontend/api/StarsButton.tsx
+++ daikoku/javascript/src/components/frontend/api/StarsButton.tsx
@@ -2,6 +2,7 @@ import { GlobalContext } from "../../../contexts/globalContext";
 import { useContext, useState } from "react";
 import { I18nContext } from "../../../contexts";
 import { Star } from "lucide-react";
+import classNames from "classnames";
 
 type StarButtonProps = {
   toggleStar: () => Promise<any>
@@ -18,11 +19,11 @@ const StarsButton = ({ toggleStar, starred, classnames }: StarButtonProps) => {
     return (
       <button
         className={`favorite-btn ${classnames ?? ''}`}
-        style={{background: 'none', border: 'none'}}
+        style={{ background: 'none', border: 'none' }}
         aria-label={translate(star ? "api.home.remove.api.to.favorite" : "api.home.add.api.to.favorite")}
         onClick={() => toggleStar().then(() => setStar(!star))}
       >
-        <Star fill={star ? "blue" : "none"} />
+        <Star className={classNames({ active: star })} size={16} strokeWidth={2} />
       </button>
     )
   }
